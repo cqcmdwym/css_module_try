ba0db032b7a3052a726e91e68838281e4cde9af1
cooper.chen <cooper.chen@ffcnn238.local>
2018-09-06 15:12:49 +0800

    loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,23 @@ const path = require('path');
 module.exports = {
     entry: './src',
     output: {
-        path: path.join(__dirname,'build'),
+        path: path.join(__dirname, 'build'),
         filename: 'bundle.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                use: [{ loader: 'babel-loader' }],
+                include: __dirname + '/src',
+            },
+            {
+                test: /\.css/,
+                use: [
+                    { loader: 'style-loader' }, 
+                    { loader: 'css-loader' }],
+                include: __dirname + '/src'
+            }
+        ],
+    }
 };
